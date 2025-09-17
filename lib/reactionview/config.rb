05a5ea1785f17ae88733d3d97da2fef6f601ee97
Marco Roth <marco.roth@intergga.ch>
2025-09-17 06:58:24 +0300

    Refactor Template Handler to pass `visitors` to `Herb::Engine` (#17)
    This pull request bumps the `herb` dependency to [`>=
    0.7.2`](https://github.com/marcoroth/herb/releases/tag/v0.7.2) (to
    address #4 and #16) and refactors the
    `ReActionView::Template::Handlers::Herb` handler to remove the `debug`
    option in favor of passing along the [new `visitors`
    option](https://github.com/marcoroth/herb/pull/502) to `Herb::Engine`.

--- lib/reactionview/config.rb
+++ lib/reactionview/config.rb
@@ -4,10 +4,12 @@ module ReActionView
   class Config
     attr_accessor :intercept_erb
     attr_accessor :debug_mode
+    attr_accessor :transform_visitors
 
     def initialize
       @intercept_erb = false
       @debug_mode = nil
+      @transform_visitors = []
     end
 
     def development?
