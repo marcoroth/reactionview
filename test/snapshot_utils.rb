c16e83b897bb82017c5ef7659846dc065eab1e9d
Marco Roth <marco.roth@intergga.ch>
2025-12-04 11:24:24 +0100

    Fix double escaping in attributes, `<script>` and `<style>` tags (#53)
    Depends on https://github.com/marcoroth/herb/pull/720
    Resolves https://github.com/marcoroth/reactionview/issues/12
    
    ---------
    
    Co-authored-by: Koji NAKAMURA <kozy4324@gmail.com>

---
+++ test/snapshot_utils.rb
@@ -0,0 +1,222 @@
+# frozen_string_literal: true
+
+# This is a copy of the SnapshotUtils from Herb:
+# https://github.com/marcoroth/herb/blob/638e09f894e4473b661ec6d345d84f5c4d17aa74/test/snapshot_utils.rb
+
+require "fileutils"
+require "readline"
+require "digest"
+require "json"
+
+def ask?(prompt = "")
+  Readline.readline("===> #{prompt}? (y/N) ", true).squeeze(" ").strip == "y"
+end
+
+module SnapshotUtils # rubocop:disable Metrics/ModuleLength
+  def assert_compiled_snapshot(source, handler: ReActionView::Template::Handlers::ERB, virtual_path: "test", format: :html, locals: [], options: {}) # rubocop:disable Metrics/ParameterLists
+    template = ActionView::Template.new(
+      source,
+      "test_template",
+      handler,
+      virtual_path: virtual_path,
+      format: format,
+      locals: locals
+    )
+
+    compiled_source = template.handler.call(template, source)
+
+    snapshot_key = JSON.generate({
+      source: source,
+      handler: handler,
+      virtual_path: virtual_path,
+      options: options,
+      locals: locals,
+      format: format,
+    })
+
+    assert_snapshot_matches(compiled_source, snapshot_key, mode: "compiled")
+
+    compiled_source
+  end
+
+  def assert_evaluated_snapshot(source, ivars: {}, options: {}, handler: ReActionView::Template::Handlers::ERB, virtual_path: "test", format: :html, locals: []) # rubocop:disable Metrics/ParameterLists,Layout/LineLength
+    template = ActionView::Template.new(
+      source,
+      "test_template",
+      handler,
+      virtual_path: virtual_path,
+      format: format,
+      locals: locals
+    )
+
+    compiled_source = template.handler.call(template, source)
+
+    lookup_context = ActionView::LookupContext.new([])
+    view_context = ActionView::Base.with_empty_template_cache.new(lookup_context, {}, nil)
+
+    ivars.each do |key, value|
+      view_context.instance_variable_set(:"@#{key}", value)
+    end
+
+    result = view_context.instance_eval(compiled_source).to_s
+
+    snapshot_key = JSON.generate({
+      source: source,
+      ivars: ivars,
+      locals: locals,
+      options: options,
+      handler: handler,
+      format: format,
+    })
+
+    assert_snapshot_matches(result, snapshot_key, mode: "evaluated")
+
+    { compiled: compiled_source, result: result }
+  end
+
+  def snapshot_changed?(content, source, options = {})
+    if snapshot_file(source, options).exist?
+      previous_content = snapshot_file(source, options).read
+
+      if previous_content == content
+        puts "\n\nSnapshot for '#{class_name} #{name}' didn't change: \n#{snapshot_file(source, options)}\n"
+        false
+      else
+        puts "\n\nSnapshot for '#{class_name} #{name}' changed:\n"
+
+        puts Difftastic::Differ.new(color: :always).diff_strings(previous_content, content)
+        puts "==============="
+        true
+      end
+    else
+      puts "\n\nSnapshot for '#{class_name} #{name}' doesn't exist at: \n#{snapshot_file(source, options)}\n"
+      true
+    end
+  end
+
+  def save_failures_to_snapshot(content, source, options = {})
+    return unless snapshot_changed?(content, source, options)
+
+    puts "\n==== [ Input for '#{class_name} #{name}' ] ====="
+    puts source
+    puts "\n\n"
+
+    if !ENV["FORCE_UPDATE_SNAPSHOTS"].nil? ||
+       ask?("Do you want to update (or create) the snapshot for '#{class_name} #{name}'?")
+
+      puts "\nUpdating Snapshot for '#{class_name} #{name}' at: \n#{snapshot_file(source, options)}\n"
+
+      FileUtils.mkdir_p(snapshot_file(source, options).dirname)
+      snapshot_file(source, options).write(content)
+
+      puts "\nSnapshot for '#{class_name} #{name}' written: \n#{snapshot_file(source, options)}\n"
+    else
+      puts "\nNot updating snapshot for '#{class_name} #{name}' at: \n#{snapshot_file(source, options)}.\n"
+    end
+  end
+
+  def assert_snapshot_matches(actual, source, options = {}, mode: nil)
+    snapshot_opts = options.dup
+    snapshot_opts[:mode] = mode if mode
+
+    assert snapshot_file(source, snapshot_opts).exist?,
+           "Expected snapshot file to exist: \n#{snapshot_file(source, snapshot_opts).to_path}"
+
+    assert_equal snapshot_file(source, snapshot_opts).read, actual
+  rescue Minitest::Assertion => e
+    save_failures_to_snapshot(actual, source, snapshot_opts) if ENV["UPDATE_SNAPSHOTS"] || ENV["FORCE_UPDATE_SNAPSHOTS"]
+
+    raise unless snapshot_file(source, snapshot_opts).exist?
+
+    if snapshot_file(source, snapshot_opts)&.read != actual
+      puts
+
+      divider = "=" * `tput cols`.strip.to_i
+
+      flunk(<<~MESSAGE)
+        \e[0m
+        #{divider}
+        #{Difftastic::Differ.new(color: :always).diff_strings(snapshot_file(source, snapshot_opts).read, actual)}
+        \e[31m#{divider}
+
+        Snapshots for "#{class_name} #{name}" didn't match.
+
+        Run the test using UPDATE_SNAPSHOTS=true to update (or create) the snapshot file for "#{class_name} #{name}"
+
+        UPDATE_SNAPSHOTS=true mtest #{e.location}
+
+        #{divider}
+        \e[0m
+      MESSAGE
+    end
+  end
+
+  def snapshot_file(source, options = {}) # rubocop:disable Metrics/MethodLength
+    test_class_name = underscore(self.class.name)
+
+    content_hash = Digest::MD5.hexdigest(source || "#{source.class}-#{source.inspect}")
+
+    test_name = sanitize_name_for_filesystem(name)
+
+    mode = options[:mode]
+    mode_suffix = mode ? "_#{mode}" : ""
+
+    opts_for_hash = options.except(:mode)
+
+    if opts_for_hash && !opts_for_hash.empty?
+      options_hash = Digest::MD5.hexdigest(opts_for_hash.inspect)
+      expected_snapshot_filename = "#{test_name}#{mode_suffix}_#{content_hash}-#{options_hash}.txt"
+    else
+      expected_snapshot_filename = "#{test_name}#{mode_suffix}_#{content_hash}.txt"
+    end
+
+    base_path = Pathname.new("test/snapshots/") / test_class_name
+    expected_snapshot_path = base_path / expected_snapshot_filename
+
+    return expected_snapshot_path if expected_snapshot_path.exist?
+
+    matching_md5_files = if opts_for_hash && !opts_for_hash.empty?
+                           Dir[base_path / "*#{mode_suffix}_#{content_hash}-#{options_hash}.txt"]
+                         else
+                           Dir[base_path / "*#{mode_suffix}_#{content_hash}.txt"]
+                         end
+
+    if matching_md5_files.any? && matching_md5_files.length == 1
+      old_file = Pathname.new(matching_md5_files.first)
+
+      return expected_snapshot_path if old_file.rename(expected_snapshot_path).zero?
+
+      return old_file
+    end
+
+    expected_snapshot_path
+  end
+
+  private
+
+  def sanitize_name_for_filesystem(name)
+    [
+      # ntfs reserved characters
+      # https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file
+      ["<", "lt"],
+      [">", "gt"],
+      [":", ""],
+      ["/", "_"],
+      ["\\", ""],
+      ["|", ""],
+      ["?", ""],
+      ["*", ""],
+
+      [" ", "_"]
+    ].inject(name) { |name, substitution| name.gsub(substitution[0], substitution[1]) }
+  end
+
+  def underscore(string)
+    string.gsub("::", "/")
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+          .tr("-", "_")
+          .tr(" ", "_")
+          .downcase
+  end
+end
--- test/template/handlers/herb_test.rb
+++ test/template/handlers/herb_test.rb
@@ -2,175 +2,88 @@
 
 require_relative "../../test_helper"
 
-class HerbTemplateHandlerTest < Minitest::Test
-  def setup
+class Herb::TemplateHandlerTest < Minitest::Spec
+  before do
     ReActionView.config.debug_mode = false
     ReActionView.config.intercept_erb = true
 
     lookup_context = ActionView::LookupContext.new([])
     @view_context = ActionView::Base.with_empty_template_cache.new(lookup_context, {}, nil)
-    @handler = ReActionView::Template::Handlers::Herb.new
   end
 
-  def test_error_for_invalid_html
-    template_source = "Plain text: <%= 1 + 1 %> (<with_an_invalid_bracket>)"
+  test "rendering for non-html formats" do
+    template = "Plain text: <%= 1 + 1 %> (<with_an_invalid_bracket>)"
 
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::ERB,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
-
-    result = template.render(@view_context, {})
-
-    assert_includes result, "data-herb-parser-error"
+    assert_compiled_snapshot(template, format: :text)
+    assert_evaluated_snapshot(template, format: :text, ivars: { name: "User" })
   end
 
-  def test_rendering_for_non_html_formats
-    template_source = "Plain text: <%= 1 + 1 %> (<with_an_invalid_bracket>)"
+  test "error for invalid html" do
+    template = "Plain text: <%= 1 + 1 %> (<with_an_invalid_bracket>)"
 
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::ERB,
-      virtual_path: "test",
-      format: :text,
-      locals: []
-    )
-
-    result = template.render(@view_context, {})
-
-    assert_equal "Plain text: 2 (<with_an_invalid_bracket>)", result
+    assert_compiled_snapshot(template, format: :html)
+    assert_evaluated_snapshot(template, format: :html, ivars: { name: "User" })
   end
 
-  def test_compilation_basic_template
-    template = "<h1>Hello <%= @name %></h1>"
+  test "template with expression" do
+    template = %(<h1>Hello <%= @name %></h1>)
 
-    herb_engine = ReActionView::Template::Handlers::Herb::Herb.new(template)
-    compiled_source = herb_engine.src
-
-    assert_includes compiled_source, "@output_buffer"
-    assert_includes compiled_source, ".append="
-    assert_includes compiled_source, "Hello "
-
-    refute_includes compiled_source, " src "
-    refute_includes compiled_source, "flush_newline_if_pending(src)"
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { name: "User" })
   end
 
-  def test_compilation_with_blocks
-    template = '<%= link_to "/users", class: "btn" do %>Click me<% end %>'
+  test "link_to with block" do
+    template = %(<%= link_to "/users", class: "btn" do %>Click me<% end %>)
 
-    herb_engine = ReActionView::Template::Handlers::Herb::Herb.new(template)
-    compiled_source = herb_engine.src
-
-    assert_includes compiled_source, 'link_to "/users", class: "btn" do'
-    refute_includes compiled_source, '(link_to "/users", class: "btn" do'
-
-    assert_includes compiled_source, "@output_buffer"
-    assert_includes compiled_source, ".safe_append="
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template)
   end
 
-  def test_compilation_escape_behavior
-    template = "<%= @html %><%= raw @safe_html %>"
+  test "raw and regular output" do
+    template = %(<%= @html %><%= raw @safe_html %>)
 
-    herb_engine = ReActionView::Template::Handlers::Herb::Herb.new(template)
-    compiled_source = herb_engine.src
-
-    assert_includes compiled_source, ".append="
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      html: "<b>bold</b>",
+      safe_html: "<i>italic</i>",
+    })
   end
 
-  def test_compilation_newline_handling
-    template = "<div>\n<%= @content %>\n</div>"
+  test "template with newlines" do
+    template = %(<div>\n<%= @content %>\n</div>)
 
-    herb_engine = ReActionView::Template::Handlers::Herb::Herb.new(template)
-    compiled_source = herb_engine.src
-
-    assert_includes compiled_source, ".safe_append="
-    assert_includes compiled_source, "<div>"
-    assert_includes compiled_source, "</div>"
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { content: "Hello" })
   end
 
-  def test_rendering_with_actionview
-    template_source = "<h1>Hello <%= @name %>!</h1><p><%= @message %></p>"
+  test "multiple expressions" do
+    template = %(<h1>Hello <%= @name %>!</h1><p><%= @message %></p>)
 
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
-
-    @view_context.instance_variable_set(:@name, "World")
-    @view_context.instance_variable_set(:@message, "Welcome to ReActionView!")
-
-    result = template.render(@view_context, {})
-
-    assert_equal "<h1>Hello World!</h1><p>Welcome to ReActionView!</p>", result
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      name: "World",
+      message: "Welcome to ReActionView!",
+    })
   end
 
-  def test_rendering_with_html_escaping
-    template_source = "<div><%= @unsafe_content %></div>"
+  test "xss protection" do
+    template = %(<div><%= @unsafe_content %></div>)
 
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
-
-    @view_context.instance_variable_set(:@unsafe_content, '<script>alert("XSS")</script>')
-
-    result = template.render(@view_context, {})
-
-    assert_equal "<div>&lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;</div>", result
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      unsafe_content: '<script>alert("XSS")</script>',
+    })
   end
 
-  def test_rendering_with_rails_helpers
-    template_source = '<%= content_tag :div, "Hello", class: "greeting" %>'
+  test "content_tag helper" do
+    template = %(<%= content_tag :div, "Hello", class: "greeting" %>)
 
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
-
-    result = template.render(@view_context, {})
-
-    assert_equal '<div class="greeting">Hello</div>', result
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template)
   end
 
-  def test_rendering_with_link_to_block
-    template_source = '<%= link_to "/users", class: "btn" do %>Click me<% end %>'
-
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
-
-    result = template.render(@view_context, {})
-
-    assert_includes result, "<a"
-    assert_includes result, 'href="/users"'
-    assert_includes result, 'class="btn"'
-    assert_includes result, ">Click me</a>"
-  end
-
-  def test_rendering_complex_template
-    template_source = <<~ERB
+  test "user card with conditional and link_to" do
+    template = <<~HTML
       <div class="user-card">
         <h2><%= @user[:name] %></h2>
         <%= if @user[:verified] %>
@@ -180,20 +93,21 @@ class HerbTemplateHandlerTest < Minitest::Test
           View Profile
         <% end %>
       </div>
-    ERB
-
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
+    HTML
 
     def @view_context.user_path(id)
       "/users/#{id}"
     end
+
+    template_obj = ActionView::Template.new(
+      template,
+      "test_template",
+      ReActionView::Template::Handlers::ERB,
+      virtual_path: "test",
+      format: :html,
+      locals: []
+    )
+    compiled_source = template_obj.handler.call(template_obj, template)
 
     @view_context.instance_variable_set(:@user, {
       name: "John Doe",
@@ -201,17 +115,16 @@ class HerbTemplateHandlerTest < Minitest::Test
       id: 123,
     })
 
-    result = template.render(@view_context, {})
+    result = @view_context.instance_eval(compiled_source).to_s
 
-    assert_includes result, '<div class="user-card">'
-    assert_includes result, "<h2>John Doe</h2>"
-    assert_includes result, '<span class="badge">Verified</span>'
-    assert_includes result, 'href="/users/123"'
-    assert_includes result, "View Profile"
+    assert_compiled_snapshot(template)
+
+    normalized_result = result.gsub(/>\s+</, "><").gsub(/\s+/, " ").strip
+    assert_equal '<div class="user-card"><h2>John Doe</h2><span class="badge">Verified</span><a class="btn btn-primary" href="/users/123"> View Profile </a></div>', normalized_result
   end
 
-  def test_rendering_complex_erb_constructs
-    template_source = <<~ERB
+  test "complex layout with helpers" do
+    template = <<~HTML
       <div class="container py-8">
         <h1 class="title"><%= title "Events by Country" %></h1>
 
@@ -225,16 +138,7 @@ class HerbTemplateHandlerTest < Minitest::Test
           <% end %>
         <% end %>
       </div>
-    ERB
-
-    template = ActionView::Template.new(
-      template_source,
-      "test_template",
-      ReActionView::Template::Handlers::Herb,
-      virtual_path: "test",
-      format: :html,
-      locals: []
-    )
+    HTML
 
     def @view_context.title(text)
       text
@@ -256,22 +160,186 @@ class HerbTemplateHandlerTest < Minitest::Test
       "<span class=\"badge\">#{count}</span>".html_safe
     end
 
-    @view_context.instance_variable_set(:@show_countries, true)
+    template_obj = ActionView::Template.new(
+      template,
+      "test_template",
+      ReActionView::Template::Handlers::ERB,
+      virtual_path: "test",
+      format: :html,
+      locals: []
+    )
+    compiled_source = template_obj.handler.call(template_obj, template)
 
-    result = template.render(@view_context, {})
+    @view_context.instance_variable_set(:@show_countries, true)
+    result = @view_context.instance_eval(compiled_source).to_s
+
+    assert_compiled_snapshot(template)
 
     assert_includes result, '<div class="container py-8">'
     assert_includes result, '<h1 class="title">Events by Country</h1>'
     assert_includes result, '<button class="btn">View all cities</button>'
-
     assert_includes result, "<h2>Countries</h2>"
-
-    assert_includes result, "<a"
     assert_includes result, 'href="/countries/switzerland"'
     assert_includes result, 'id="country-ch"'
     assert_includes result, 'class="event-item"'
     assert_includes result, ">🇨🇭 Switzerland</span>"
     assert_includes result, '<span class="badge">5</span>'
-    assert_includes result, "</a>"
+  end
+
+  test "combobox data attribute with string array" do
+    template = %(<div data-controller="combobox" data-combobox-choices-value="<%= @choices.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { choices: ["Volkslied", "Weihnachtslied", "foo"] })
+  end
+
+  test "data attribute with json_escape helper" do
+    template = %(<div data-holidays-current-value="<%= json_escape(@holidays.to_json) %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { holidays: ["2025-10-20", "2025-10-21", "2025-11-26"] })
+  end
+
+  test "data attribute with nested object" do
+    template = %(<div data-config="<%= @config.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { config: { key: "value" } })
+    assert_evaluated_snapshot(template, ivars: {
+      config: {
+        name: "Test",
+        options: { enabled: true, count: 42 },
+        items: ["a", "b"],
+      },
+    })
+  end
+
+  test "special html characters in json" do
+    template = %(<div data-value="<%= @value.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { value: ["<script>", "a & b", "x > y"] })
+  end
+
+  test "script with raw helper and json" do
+    template = %(<script>window.config = <%= raw @config.to_json %></script>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { config: { key: "value", number: 123 } })
+  end
+
+  test "script with application json type" do
+    template = %(<script type="application/json"><%= @data.to_json.html_safe %></script>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { data: { items: ["one", "two"], count: 2 } })
+  end
+
+  test "style with multiple custom properties" do
+    template = %(<style>:root { --primary: <%= @primary %>; --secondary: <%= @secondary %>; }</style>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      primary: "#FF0000",
+      secondary: "rgb(0, 255, 0)",
+    })
+  end
+
+  test "empty json structures" do
+    template = %(<div data-empty-array="<%= @empty_array.to_json %>" data-empty-object="<%= @empty_object.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      empty_array: [],
+      empty_object: {},
+    })
+  end
+
+  test "json with primitive types" do
+    template = %(<div data-values="<%= @values.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      values: {
+        active: true,
+        disabled: false,
+        count: 42,
+        price: 19.99,
+        nothing: nil,
+      },
+    })
+  end
+
+  test "script with javascript expression" do
+    template = %(<script>const data = <%= @data.to_json.html_safe %>; console.log(data);</script>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { data: { message: "Hello, World!" } })
+  end
+
+  test "data attributes with json" do
+    template = %(<div data-value="<%= @data.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { data: ["foo", "bar"] })
+  end
+
+  test "script tag with json" do
+    template = %(<script>window.data = <%= @data.to_json.html_safe %></script>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { data: { key: "value", items: [1, 2, 3] } })
+  end
+
+  test "multiline script with hash" do
+    template = <<~HTML
+      <script>
+        window.railsVariables = <%= @hash.to_json.html_safe %>
+      </script>
+    HTML
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { hash: { a: "first_item", b: "second_item" } })
+  end
+
+  test "canvas with boolean array and multiple attributes" do
+    template = <<~HTML
+      <canvas
+        data-controller="chart"
+        data-chart-unit-type-value="<%= @unit_type %>"
+        data-chart-data-value="<%= @data.to_json %>"
+        data-chart-labels-value="<%= @labels.to_json %>"
+        data-chart-unit-value="<%= @unit_label %>"
+      ></canvas>
+    HTML
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: {
+      unit_type: "yes/no",
+      data: [true, true, true, true, true, true],
+      labels: ["2025-05-16", "2025-05-17", "2025-05-19", "2025-05-20", "2025-07-28", "2025-09-12"],
+      unit_label: "sessions",
+    })
+  end
+
+  test "concat" do
+    template = %(<div data-json="<% concat(@data.to_json) %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { data: { a: 1 } })
+  end
+
+  test "raw helper with json" do
+    template = %(<div data-config="<%= raw @config.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { config: { key: "value" } })
+  end
+
+  test "raw output" do
+    template = %(<div data-config="<%== @config.to_json %>"></div>)
+
+    assert_compiled_snapshot(template)
+    assert_evaluated_snapshot(template, ivars: { config: { key: "value" } })
   end
 end
