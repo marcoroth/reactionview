fbdc8a95f3343847460ba35fdcffeed5ddfaf153
Marco Roth <marco.roth@intergga.ch>
2026-08-08 03:34:22 +0200

    Skip templates from gems vendored inside the application

--- lib/reactionview/template/handlers/erb.rb
+++ lib/reactionview/template/handlers/erb.rb
@@ -22,8 +22,15 @@ module ReActionView
 
         def local_template?(template)
           return true unless template.respond_to?(:identifier) && template.identifier
+          return false if vendored_template?(template)
 
           template.identifier.start_with?(Rails.root.to_s)
+        end
+
+        def vendored_template?(template)
+          return false unless defined?(Bundler)
+
+          template.identifier.start_with?(Bundler.bundle_path.to_s)
         end
       end
     end
