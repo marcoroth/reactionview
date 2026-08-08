83726df97c144ffb52b6bd7bc19513d77aae0588
Marco Roth <marco.roth@intergga.ch>
2026-08-08 04:44:49 +0200

    Revert "Fix: Use Rails::Engine for Propshaft compatibility"
    This reverts commit 2eee4b8e6674c3c789c012f2c26a9275baa83171.

--- lib/reactionview.rb
+++ lib/reactionview.rb
@@ -21,7 +21,7 @@ require_relative "reactionview/template/handlers/erb"
 require_relative "reactionview/template/handlers/herb"
 require_relative "reactionview/template/handlers/herb/herb"
 
-require_relative "reactionview/engine" if defined?(Rails::Engine)
+require_relative "reactionview/railtie" if defined?(Rails::Railtie)
 
 module ReActionView
 end
